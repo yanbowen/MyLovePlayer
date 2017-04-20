d5c129e49f5a33657ee669a8a48543ff87330454
闫博文 <yanbowen90@outlook.com>
2017-04-20 22:57:22 +0800

    Initial commit

---
+++ MyLoveSunLin/src/main/java/cn/studyjams/s2/sj0132/bowenyan/myloveplayer/AndyViewPagerActivity.java
@@ -0,0 +1,153 @@
+package cn.studyjams.s2.sj0132.bowenyan.myloveplayer;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by yanbowen on 4/20/2017.
+ */
+
+public class AndyViewPagerActivity extends Activity implements View.OnClickListener, ViewPager.OnPageChangeListener {
+
+    private ViewPager vp;
+    private ViewPagerAdapter vpAdapter;
+    private List<View> views;
+    private Button button;
+
+    //引导图片资源
+    private static final int[] pics = {R.drawable.guide1,
+            R.drawable.guide2, R.drawable.guide3,
+            R.drawable.guide4};
+
+    //底部小店图片
+    private ImageView[] dots;
+
+    //记录当前选中位置
+    private int currentIndex;
+
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcome);
+        button = (Button) findViewById(R.id.button);
+        views = new ArrayList<View>();
+
+        LinearLayout.LayoutParams mParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+
+        //初始化引导图片列�?
+        for (int i = 0; i < pics.length; i++) {
+            ImageView iv = new ImageView(this);
+            iv.setLayoutParams(mParams);
+            iv.setImageResource(pics[i]);
+            views.add(iv);
+        }
+        vp = (ViewPager) findViewById(R.id.viewpager);
+        //初始化Adapter
+        vpAdapter = new ViewPagerAdapter(views);
+        vp.setAdapter(vpAdapter);
+        //绑定回调
+        vp.setOnPageChangeListener(this);
+//        button = (Button) findViewById(R.id.button);
+        //初始化底部小
+        initDots();
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(AndyViewPagerActivity.this, MainActivity.class);
+                AndyViewPagerActivity.this.startActivity(intent);
+                finish();
+            }
+        });
+
+    }
+
+    private void initDots() {
+        LinearLayout ll = (LinearLayout) findViewById(R.id.ll);
+
+        dots = new ImageView[pics.length];
+
+        //循环取得小点图片
+        for (int i = 0; i < pics.length; i++) {
+            dots[i] = (ImageView) ll.getChildAt(i);
+            dots[i].setEnabled(true);//都设为灰�?
+            dots[i].setOnClickListener((View.OnClickListener) this);
+            dots[i].setTag(i);//设置位置tag，方便取出与当前位置对应
+        }
+
+        currentIndex = 0;
+        dots[currentIndex].setEnabled(false);//设置为白色，即�?中状�?
+    }
+
+    /**
+     * 设置当前的引导页
+     */
+    private void setCurView(int position) {
+        if (position < 0 || position >= pics.length) {
+            return;
+        }
+
+        vp.setCurrentItem(position);
+    }
+
+    /**
+     * 这只当前引导小点的�?�?
+     */
+    private void setCurDot(int positon) {
+        if (positon < 0 || positon > pics.length - 1 || currentIndex == positon) {
+            return;
+        }
+
+        dots[positon].setEnabled(false);
+        dots[currentIndex].setEnabled(true);
+
+        currentIndex = positon;
+    }
+
+    //当滑动状态改变时调用
+    @Override
+    public void onPageScrollStateChanged(int arg0) {
+        // TODO Auto-generated method stub
+
+    }
+
+    //当当前页面被滑动时调�?
+    @Override
+    public void onPageScrolled(int arg0, float arg1, int arg2) {
+        // TODO Auto-generated method stub
+
+    }
+
+    //当新的页面被选中时调�?
+    @Override
+    public void onPageSelected(int arg0) {
+        //设置底部小点选中状�?
+        setCurDot(arg0);
+        if (arg0 == 3) {
+            button.setVisibility(View.VISIBLE);
+
+        } else {
+            button.setVisibility(View.GONE);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        int position = (Integer) v.getTag();
+        setCurView(position);
+        setCurDot(position);
+    }
+}
---
+++ MyLoveSunLin/src/main/java/cn/studyjams/s2/sj0132/bowenyan/myloveplayer/MainActivity.java
@@ -0,0 +1,13 @@
+package cn.studyjams.s2.sj0132.bowenyan.myloveplayer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
